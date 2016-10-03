8c2fc9f84b28af78830272817e2337a1ed636544
Lisa Zorn <lzorn@mtc.ca.gov>
2016-10-03 14:36:03 -0700

    Update overlap script to use different inputs and include test size

--- scripts/testOverlap.bat
+++ scripts/testOverlap.bat
@@ -3,13 +3,14 @@
 ::
 
 :: first, setup for run
-set NETWORK_DIR=sfcta\network_draft1.8
-set DEMAND_DIR=sfcta\CHTS_fasttrips_demand_v0.2
+set NETWORK_DIR=sfcta\network_draft1.9
+set DEMAND_DIR=sfcta\CHAMP_fasttrips_demand_v0.3
+set TEST_SIZE=1000
 
-goto overlap_split_test
+:: goto overlap_split_test
 
 :: run with pathfinding
-python .\scripts\runTest.py stochastic 1 "%NETWORK_DIR%" "%DEMAND_DIR%" sfcta
+python .\scripts\runTest.py --num_trips %TEST_SIZE% stochastic 1 "%NETWORK_DIR%" "%DEMAND_DIR%" sfcta
 IF ERRORLEVEL 1 goto done
 
 :overlap_test
@@ -17,18 +18,18 @@ IF ERRORLEVEL 1 goto done
 for %%A in (None count distance time) do (
 
   rem run again without pathfinding
-  mkdir sfcta\CHTS_fasttrips_demand_v0.2_file_iter1_nocap
+  mkdir "%DEMAND_DIR%_file_iter1_nocap"
 
   rem copy pathfinding files in place
-  copy  sfcta\CHTS_fasttrips_demand_v0.2_stochastic_iter1_nocap\pathsfound_paths.csv sfcta\CHTS_fasttrips_demand_v0.2_file_iter1_nocap
-  copy  sfcta\CHTS_fasttrips_demand_v0.2_stochastic_iter1_nocap\pathsfound_links.csv sfcta\CHTS_fasttrips_demand_v0.2_file_iter1_nocap
+  copy  "%DEMAND_DIR%_stochastic_iter1_nocap\pathsfound_paths.csv" "%DEMAND_DIR%_file_iter1_nocap"
+  copy  "%DEMAND_DIR%_stochastic_iter1_nocap\pathsfound_links.csv" "%DEMAND_DIR%_file_iter1_nocap"
 
   rem do the test
-  python .\scripts\runTest.py --overlap_variable %%A file 1 "%NETWORK_DIR%" "%DEMAND_DIR%" sfcta
+  python .\scripts\runTest.py --num_trips %TEST_SIZE% --overlap_variable %%A file 1 "%NETWORK_DIR%" "%DEMAND_DIR%" sfcta
   IF ERRORLEVEL 1 goto done
 
   rem save results
-  move sfcta\CHTS_fasttrips_demand_v0.2_file_iter1_nocap sfcta\CHTS_fasttrips_demand_v0.2_file_iter1_nocap_overlap_%%A
+  move "%DEMAND_DIR%_file_iter1_nocap" "%DEMAND_DIR%_file_iter1_nocap_overlap_%%A"
 )
 
 :: one more time but with split transit
@@ -37,17 +38,17 @@ for %%A in (None count distance time) do (
 for %%A in (count distance time) do (
 
   rem run again without pathfinding
-  mkdir sfcta\CHTS_fasttrips_demand_v0.2_file_iter1_nocap
+  mkdir "%DEMAND_DIR%_file_iter1_nocap"
 
   rem copy pathfinding files in place
-  copy  sfcta\CHTS_fasttrips_demand_v0.2_stochastic_iter1_nocap\pathsfound_paths.csv sfcta\CHTS_fasttrips_demand_v0.2_file_iter1_nocap
-  copy  sfcta\CHTS_fasttrips_demand_v0.2_stochastic_iter1_nocap\pathsfound_links.csv sfcta\CHTS_fasttrips_demand_v0.2_file_iter1_nocap
+  copy  "%DEMAND_DIR%_stochastic_iter1_nocap\pathsfound_paths.csv" "%DEMAND_DIR%_file_iter1_nocap"
+  copy  "%DEMAND_DIR%_stochastic_iter1_nocap\pathsfound_links.csv" "%DEMAND_DIR%_file_iter1_nocap"
 
   rem do the test
-  python .\scripts\runTest.py --overlap_variable %%A --overlap_split_transit file 1 "%NETWORK_DIR%" "%DEMAND_DIR%" sfcta
+  python .\scripts\runTest.py --num_trips %TEST_SIZE% --overlap_variable %%A --overlap_split_transit file 1 "%NETWORK_DIR%" "%DEMAND_DIR%" sfcta
   IF ERRORLEVEL 1 goto done
 
   rem save results
-  move sfcta\CHTS_fasttrips_demand_v0.2_file_iter1_nocap sfcta\CHTS_fasttrips_demand_v0.2_file_iter1_nocap_overlap_%%A_split
+  move "%DEMAND_DIR%_file_iter1_nocap" "%DEMAND_DIR%_file_iter1_nocap_overlap_%%A_split"
 )
 :done
