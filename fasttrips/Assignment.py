b68c7f51f4f46055bda23a2dcdcf9b3dfabcaf1e
Lisa Zorn <lzorn@mtc.ca.gov>
2017-05-09 09:44:46 -0700

    Doc update

--- fasttrips/Assignment.py
+++ fasttrips/Assignment.py
@@ -1,4 +1,4 @@
-__copyright__ = "Copyright 2015 Contributing Entities"
+__copyright__ = "Copyright 2015-2017 Contributing Entities"
 __license__   = """
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
