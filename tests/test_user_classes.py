d2327ccb31414973e7d429000dfbf8f3f70d4677
Lisa Zorn <lzorn@mtc.ca.gov>
2017-05-11 11:47:21 -0700

    Make test_user_classes run with 1 process

--- tests/test_user_classes.py
+++ tests/test_user_classes.py
@@ -15,17 +15,18 @@ def test_user_classes():
     OUTPUT_DIR     = os.path.join(EXAMPLES_DIR,"output")
 
     r = Run.run_fasttrips(
-        input_network_dir= INPUT_NETWORKS,
-        input_demand_dir = INPUT_DEMAND,
-        run_config       = os.path.join(INPUT_DEMAND,"config_ft.txt"),
-        input_weights    = os.path.join(INPUT_DEMAND,"pathweight_ft.txt"),
-        input_functions  = os.path.join(INPUT_DEMAND,"config_ft.py"),
-        output_dir       = OUTPUT_DIR,
-        output_folder    = "test_userclasses",
-        pathfinding_type = "stochastic",
-        iters            = 1,
-        dispersion       = 0.50,
-        test_size        = 100)
+        input_network_dir   = INPUT_NETWORKS,
+        input_demand_dir    = INPUT_DEMAND,
+        run_config          = os.path.join(INPUT_DEMAND,"config_ft.txt"),
+        input_weights       = os.path.join(INPUT_DEMAND,"pathweight_ft.txt"),
+        input_functions     = os.path.join(INPUT_DEMAND,"config_ft.py"),
+        output_dir          = OUTPUT_DIR,
+        output_folder       = "test_userclasses",
+        pathfinding_type    = "stochastic",
+        iters               = 1,
+        dispersion          = 0.50,
+        test_size           = 100,
+        number_of_processes = 1)
     
     assert r["passengers_arrived"] > 0
 
