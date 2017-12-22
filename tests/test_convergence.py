99be8347ec0f4dc0c24b39cb25ba0339f15f1372
Clint Daniels <clint.daniels@rsginc.com>
2017-12-22 13:59:10 -0800

    Clean test formatting.

--- tests/test_convergence.py
+++ tests/test_convergence.py
@@ -1,18 +1,17 @@
 import os
-import pytest
 
 import zipfile
 
 from fasttrips import Run
 
-"test_scenario"
 
-EXAMPLES_DIR = os.path.join(os.getcwd(), "fasttrips", "Examples", )
-INPUT_NETWORKS = os.path.join(EXAMPLES_DIR, "networks")
-INPUT_DEMAND = os.path.join(EXAMPLES_DIR, "test_scenario", "demand_twopaths")
-OUTPUT_DIR = os.path.join(EXAMPLES_DIR, "test_convergence")
+def run_convergence():
+    EXAMPLES_DIR = os.path.join(os.getcwd(), "fasttrips", "Examples")
 
-def test_run_capacity_test():
+    INPUT_NETWORKS = os.path.join(EXAMPLES_DIR, "networks")
+    INPUT_DEMAND = os.path.join(EXAMPLES_DIR, "demand", "demand_twopaths")
+    OUTPUT_DIR = os.path.join(EXAMPLES_DIR, "output")
+
     scenario_dir = os.path.join(INPUT_NETWORKS, 'simple')
     scenario_file = os.path.join(INPUT_NETWORKS, 'simple.zip')
     with zipfile.ZipFile(scenario_file, 'w') as zipf:
@@ -20,20 +19,22 @@ def test_run_capacity_test():
             for file in files:
                 zipf.write(os.path.join(root, file), file)
 
-    performance = (OUTPUT_DIR, Run.run_fasttrips(
+    Run.run_fasttrips(
         input_network_dir=scenario_file,
         input_demand_dir=INPUT_DEMAND,
         run_config=os.path.join(INPUT_DEMAND, "config_ft.txt"),
         input_functions=os.path.join(INPUT_DEMAND, 'config_ft.py'),
         input_weights=os.path.join(INPUT_DEMAND, "pathweight_ft.txt"),
         output_dir=OUTPUT_DIR,
-        output_folder="output",
+        output_folder="test_convergence",
         pathfinding_type="stochastic",
         capacity=True,
         iters=4,
-        dispersion=0.50))
+        dispersion=0.50
+    )
 
 
     os.unlink(scenario_file)
 
-    return performance
+if __name__ == '__main__':
+    run_convergence()
