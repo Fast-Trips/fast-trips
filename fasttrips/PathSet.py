4b82c21ea2f776716960ec7dd73d27fef902bb43
Clint Daniels <clint.daniels@rsginc.com>
2018-01-30 13:46:58 -0800

    Intermediate commit.

--- fasttrips/PathSet.py
+++ fasttrips/PathSet.py
@@ -87,6 +87,9 @@ class PathSet:
     #: Allow departures and arrivals before / after preferred time
     ARRIVE_LATE_ALLOWED_MIN         = datetime.timedelta(minutes = 0)
     DEPART_EARLY_ALLOWED_MIN        = datetime.timedelta(minutes = 0)
+    LEARN_ROUTES                    = True
+    LEARN_ROUTES_RATE               = 0.006
+
 
     CONSTANT_GROWTH_MODEL            = 'constant'
     EXP_GROWTH_MODEL                 = 'exponential'
@@ -1274,10 +1277,9 @@ class PathSet:
                                                    Passenger.PF_COL_PATH_NUM])
 
         if PathSet.LEARN_ROUTES:
-            pathset_paths_df['learn_discount'] = (1 + PathSet.LEARN_ROUTES_RATE) ** pathset_paths_df[PathSet.SUCCESS_FLAG_COLUMN] - 1
+            pathset_paths_df['learn_discount'] = np.exp(-PathSet.LEARN_ROUTES_RATE * pathset_paths_df[PathSet.SUCCESS_FLAG_COLUMN])
             pathset_paths_df['orig_sim_cost'] = pathset_paths_df[Assignment.SIM_COL_PAX_COST]
-            pathset_paths_df[Assignment.SIM_COL_PAX_COST] = pathset_paths_df[Assignment.SIM_COL_PAX_COST] * \
-                                                            (1 - pathset_paths_df['learn_discount'])
+            pathset_paths_df[Assignment.SIM_COL_PAX_COST] = pathset_paths_df[Assignment.SIM_COL_PAX_COST] * pathset_paths_df['learn_discount']
 
         if len(Assignment.TRACE_IDS) > 0:
             FastTripsLogger.debug("calculate_cost: pathset_paths_df trace\n%s" % str(pathset_paths_df.loc[pathset_paths_df[Passenger.TRIP_LIST_COLUMN_TRACE]==True]))
