d7a8cc0436fa653b15e849106790e78af7d38212
Clint Daniels <clint.daniels@rsginc.com>
2018-01-30 17:29:25 -0800

    Working Convergence Snapshot with Learning.

--- fasttrips/PathSet.py
+++ fasttrips/PathSet.py
@@ -101,7 +101,7 @@ class PathSet:
     ]
 
     LEARN_ROUTES                    = True
-    LEARN_ROUTES_RATE               = 0.006
+    LEARN_ROUTES_RATE               = 0.05
 
 
     #: Weights column: User Class
@@ -1272,8 +1272,7 @@ class PathSet:
                                                    Passenger.PF_COL_PATH_NUM])
 
         if PathSet.LEARN_ROUTES:
-            pathset_paths_df['learn_discount'] = (1 + PathSet.LEARN_ROUTES_RATE) ** pathset_paths_df['success_flag']# * \
-#                                                 (1 - (PathSet.LEARN_ROUTES_RATE * 4)) ** pathset_paths_df['bump_flag']) - 1
+            pathset_paths_df['learn_discount'] = (1 + PathSet.LEARN_ROUTES_RATE) ** pathset_paths_df['success_flag'] - 1
             pathset_paths_df['orig_sim_cost'] = pathset_paths_df[Assignment.SIM_COL_PAX_COST]
             pathset_paths_df[Assignment.SIM_COL_PAX_COST] = pathset_paths_df[Assignment.SIM_COL_PAX_COST] * \
                                                             (1 - pathset_paths_df['learn_discount'])
