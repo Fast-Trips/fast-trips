7cea549de9a5b1458ee75b4b1486943e1f86d82c
Clint Daniels <clint.daniels@rsginc.com>
2018-02-05 17:05:46 -0800

    Correct error with conftest gtfs feed reader.

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,24 @@ from fasttrips.Util import Util
 HOME_DIR = os.path.join(os.getcwd(), "fasttrips", "Examples", )
 NETWORK_HOME_DIR = os.path.join(HOME_DIR, 'networks')
 
+
+@pytest.fixture(scope="function")
+def gtfs_feed(network, network_date):
+    from fasttrips.Assignment import Assignment
+    Assignment.NETWORK_BUILD_DATE = network_date
+    network_dir = os.path.join(NETWORK_HOME_DIR, network)
+
+    service_ids_by_date = ptg.read_service_ids_by_date(network_dir)
+    service_ids = service_ids_by_date[network_date]
+    feed = ptg.feed(network_dir,
+                          config=Util.get_fast_trips_config(), view={
+        'trips.txt': {
+            'service_id': service_ids
+        },
+    })
+    yield feed
+
+
 @pytest.fixture(scope="module", params=["simple", "psrc_1_1"])
 def network(request):
     yield request.param
