fa7803ab554950b02ef060fadb67691635f02370
Clint Daniels <clint.daniels@rsginc.com>
2018-02-08 14:46:55 -0800

    Exchange test loops with pytest fixture management.

--- tests/test_maxStopProcessCount.py
+++ tests/test_maxStopProcessCount.py
@@ -1,9 +1,26 @@
 import os
+import pytest
 
 from fasttrips import Run
 
 
-def test_max_stop_process_count():
+@pytest.fixture(scope='module', params=[10, 50, 100])
+def stop_process_count(request):
+    return request.param
+
+
+@pytest.fixture(scope='module')
+def passengers_arrived(stop_process_count):
+    arrived = {
+        10: 726,
+        50: 726,
+        100: 726,
+    }
+
+    return arrived[stop_process_count]
+
+
+def test_max_stop_process_count(stop_process_count, passengers_arrived):
 
     EXAMPLES_DIR   = os.path.join(os.getcwd(), "fasttrips", "Examples")
 
@@ -11,20 +28,16 @@ def test_max_stop_process_count():
     INPUT_DEMAND   = os.path.join(EXAMPLES_DIR, 'demand', "demand_reg")
     OUTPUT_DIR     = os.path.join(EXAMPLES_DIR, "output")
 
-    for max_spc in [10, 50, 100]: 
+    r = Run.run_fasttrips(
+        input_network_dir = INPUT_NETWORK,
+        input_demand_dir  = INPUT_DEMAND,
+        run_config        = os.path.join(INPUT_DEMAND, "config_ft.txt"),
+        input_weights     = os.path.join(INPUT_DEMAND, "pathweight_ft.txt"),
+        output_dir        = OUTPUT_DIR,
+        output_folder     = "test_dispers_%4.2d" % stop_process_count,
+        pathfinding_type  = "stochastic",
+        max_stop_process_count = stop_process_count,
+        iters             = 1,
+        dispersion        = 0.50 )
 
-        r = Run.run_fasttrips(
-            input_network_dir= INPUT_NETWORK,
-            input_demand_dir = INPUT_DEMAND,
-            run_config       = os.path.join(INPUT_DEMAND, "config_ft.txt"),
-            input_weights    = os.path.join(INPUT_DEMAND, "pathweight_ft.txt"),
-            output_dir       = OUTPUT_DIR,
-            output_folder    = "test_dispers_%4.2d" % max_spc,
-            pathfinding_type = "stochastic",
-            max_stop_process_count = max_spc,
-            iters            = 1,
-            dispersion       = 0.50 )
-        
-        assert r["passengers_arrived"] > 0
-        assert r["capacity_gap"]      < 0.001
-        assert r["passengers_missed"] == 0
+    assert passengers_arrived == r["passengers_arrived"]
