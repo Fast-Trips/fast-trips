5605689b23592393a92d634e6b7d4779ccd30420
Clint Daniels <clint.daniels@rsginc.com>
2018-02-08 14:46:55 -0800

    Exchange test loops with pytest fixture management.

--- tests/test_dispersion.py
+++ tests/test_dispersion.py
@@ -2,9 +2,24 @@ import os
 import pytest
 from fasttrips import Run
 
-@pytest.mark.parametrize("dispersion_parameter", [1.0,0.3])
 
-def test_dispersion(dispersion_parameter):
+@pytest.fixture(scope='module', params=[1.0, 0.7, 0.5, 0.4, 0.1])
+def dispersion_rate(request):
+    return request.param
+
+@pytest.fixture(scope='module')
+def passengers_arrived(dispersion_rate):
+    arrived = {
+        1.0: 723,
+        0.7: 726,
+        0.5: 726,
+        0.4: 726,
+        0.1: 726,
+    }
+
+    return arrived[dispersion_rate]
+
+def test_dispersion(dispersion_rate, passengers_arrived):
 
     EXAMPLES_DIR   = os.path.join(os.getcwd(), "fasttrips", "Examples",)
 
@@ -12,27 +27,25 @@ def test_dispersion(dispersion_parameter):
     INPUT_DEMAND   = os.path.join(EXAMPLES_DIR, 'demand', "demand_reg")
     OUTPUT_DIR     = os.path.join(EXAMPLES_DIR, "output")
 
-    full_output_dir = os.path.join(OUTPUT_DIR, "test_dispers_%4.2f" % dispersion_parameter)
+    full_output_dir = os.path.join(OUTPUT_DIR, "test_dispers_%4.2f" % dispersion_rate)
     if not os.path.exists(full_output_dir):
         os.mkdir(full_output_dir)
 
     r = Run.run_fasttrips(
-        input_network_dir= INPUT_NETWORK,
-        input_demand_dir = INPUT_DEMAND,
-        run_config       = os.path.join(INPUT_DEMAND,"config_ft.txt"),
-        input_weights    = os.path.join(INPUT_DEMAND,"pathweight_ft.txt"),
-        output_dir       = OUTPUT_DIR,
+        input_network_dir = INPUT_NETWORK,
+        input_demand_dir  = INPUT_DEMAND,
+        run_config        = os.path.join(INPUT_DEMAND,"config_ft.txt"),
+        input_weights     = os.path.join(INPUT_DEMAND,"pathweight_ft.txt"),
+        output_dir        = OUTPUT_DIR,
         max_stop_process_count = 2,
         pf_iters         = 2,
         overlap_variable = "None",
-        output_folder    = "test_dispers_%4.2f" % dispersion_parameter,
-        pathfinding_type = "stochastic",
-        iters            = 1,
-        dispersion       = dispersion_parameter,
-        test_size        = 2)
+        output_folder    = "test_dispers_%4.2f" % dispersion_rate,
+        pathfinding_type  = "stochastic",
+        iters             = 1,
+        dispersion        = dispersion_rate,
+        test_size         = 100 )
 
-    assert r["passengers_arrived"] > 0
-
-if __name__ == "__main__":
+    assert passengers_arrived == r["passengers_arrived"]
     for dp in [1.0,0.3]:
         test_dispersion(dp)
